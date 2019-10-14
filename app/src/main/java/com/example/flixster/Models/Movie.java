a1ecc7bad008df6a59d6cb87354be168af76389e
Emma Neary <nearyemm@gmail.com>
2019-10-14 14:26:05 -0400

    Network request to get movie data implemented

---
+++ app/src/main/java/com/example/flixster/Models/Movie.java
@@ -0,0 +1,41 @@
+package com.example.flixster.Models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+
+    String title;
+    String poster;
+    String summary;
+
+    public Movie(JSONObject jsonobject) throws JSONException {
+        poster = jsonobject.getString("poster_path");
+        title = jsonobject.getString("title");
+        summary = jsonobject.getString("overview");
+    }
+
+    public static List<Movie> getMovieList(JSONArray arr) throws JSONException {
+        List<Movie> movies = new ArrayList<Movie>();
+        for (int i = 0; i < arr.length(); i++){
+            movies.add(new Movie(arr.getJSONObject(i)));
+        }
+        return movies;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPoster() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", poster);
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+}
